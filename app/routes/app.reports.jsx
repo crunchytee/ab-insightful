6c6a8005469f468830bad3aaaa8368393fbf8139
ryanmart25 <80070431+ryanmart25@users.noreply.github.com>
2025-10-15 15:16:01 -0700

    feat: added buttons to SB nav. defined routes.

---
+++ app/routes/app.reports.jsx
@@ -0,0 +1,9 @@
+export default function Reports() {
+    return (
+        <s-page heading="Reports">
+            <s-secton>
+                <s-paragraph>An empty card</s-paragraph>
+            </s-secton>
+        </s-page>
+    );
+}
