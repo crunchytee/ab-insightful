b2a945380e2559f4ce6129c7ac9b088f2b5dc48c
Ryan Martinez <ryanmartinez2021@gmail.com>
2025-10-15 17:50:02 -0700

    feat: completed empty state of app.experiments.jsx

--- app/routes/app.additional.jsx
+++ app/routes/app.additional.jsx
@@ -2,20 +2,27 @@ export default function ExperimentManagement() {
   // this file represents the route for /experiments/create, as needed by ET-20. The "Create Experiment" button navigates to this route, and shows the page defined by this file.  
   return (
     <s-page heading="Experiment Management">
-      <s-stack>
-              <s-section heading="Experiment List">
-          <s-grid justifyItems="center">
-            <s-box  maxInlineSize="266px" maxBlockSize="266px">
-              <s-image src="public\Group-182.svg"></s-image>
-            </s-box>
+      <s-section accessibilityLabel="Empty state section">
+        <s-grid gap="base" justifyItems="center" paddingBlock="large-500">
+          <s-box maxInlineSize="400px" maxBlockSize="400px">
+            <s-image
+              aspectRatio="1/1.5"
+              src="/Group-182.svg"
+              alt="Empty state image"
+            />
+          </s-box>
+          <s-grid justifyItems="center" maxInlineSize="450px" gap="base">
+            <s-stack alignItems="center">
+              <s-heading>Your experiments will show here</s-heading>
+              <s-paragraph>
+                This is where you will examine and select from your list of experiments. 
+              </s-paragraph>
+                            <s-button variant="primary">Create Experiment</s-button>
+            </s-stack>
+              <s-button >Create Experiment</s-button>
           </s-grid>
-          <s-stack alignItems="center">
-            <s-heading>Your experiments will show here</s-heading>
-            <s-paragraph> This is where you will examine and select from your list of experiments</s-paragraph>
-            <s-button href="/app/experiments/new" variant="primary">Create Experiment</s-button>
-          </s-stack>
-        </s-section>
-      </s-stack>
+        </s-grid>
+      </s-section>
 
     </s-page>
   );
