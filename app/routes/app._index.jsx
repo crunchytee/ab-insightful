3c713920ea8599ea18e6fb429389d45dd1d0abcb
Ryan Martinez <ryanmartinez2021@gmail.com>
2025-10-26 13:27:28 -0700

    potential fix: auto registering pixel

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -5,8 +5,49 @@ import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
+  const {admin} = await authenticate.admin(request);
+  // [ryan] Code for automatically registering the Web-Pixel extension with the shop. 
+  // I'm not sure if this is the right place to do it, but my gut tells me it needs to happen on app load, 
+  // and this is as early as I can imagine it needs to be. 
+  
+  // im kinda just following the pattern below me in the action() function. 
+  // check for web-pixels that already exist on this store
 
+  const exists = await admin.graphql(
+    `#graphql
+      query {
+        shop { webPixels(first: 1) { edges { node { id settings } } } }
+      }
+  `
+  );
+
+  const existsAsJSON = await exists.json();
+  const alreadyExists = existsAsJSON?.data?.shop?.webPixels?.edges?.length > 0;
+  
+  if (!alreadyExists){ // register if there are no other web-pixels. 
+    const response = await admin.graphql(
+      `#graphql
+        mutation {
+        webPixelCreate(webPixel: { settings: "{"accountID":"123"}" }) {
+          userErrors {
+            code
+            field
+            message
+          }
+          webPixel {
+            settings
+            id
+          }
+        }
+      }
+  `
+    );
+    const responseAsJSON = await response.json()
+    // how do i check for errors? user errors won't be populated when it succeeds right? 
+    if(responseAsJSON.data?.webPixelCreate?.userErrors?.length > 0){
+      console.error('An error occurred while trying to register the Web Pixel App Extension:', responseAsJSON.data.webPixelCreate.userErrors)
+    }
+  }
   return null;
 };
 
