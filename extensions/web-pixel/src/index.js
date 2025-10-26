7adde5655bb5ec559cb49ae45c6e63a417339b80
Ryan Martinez <ryanmartinez2021@gmail.com>
2025-10-26 13:20:50 -0700

    fixed indendation in web-pixel/index.js

--- extensions/web-pixel/src/index.js
+++ extensions/web-pixel/src/index.js
@@ -27,54 +27,53 @@ register(({ analytics, browser, init, settings }) => {
     }
     console.log('[Pixel] product viewed ', payload)
   })
-    console.log("Hello world from the Pixel extension")
-    // determine what the "resource" at this page is. 
-    // parse the location attribute, try to match it against the registered
-    // resources in associated_resources_search 
-    analytics.subscribe('page_viewed', (event) => {
-      console.log(event)
-      let resource = associated_resources_search.OTHER;
-      for (const key in associated_resources_search){
-        if(event.context.document.location.href.includes(associated_resources_search[key])){
-          resource = associated_resources_search[key];
-        }
+  console.log("Hello world from the Pixel extension")
+  // determine what the "resource" at this page is. 
+  // parse the location attribute, try to match it against the registered
+  // resources in associated_resources_search 
+  analytics.subscribe('page_viewed', (event) => {
+    console.log(event)
+    let resource = associated_resources_search.OTHER;
+    for (const key in associated_resources_search){
+      if(event.context.document.location.href.includes(associated_resources_search[key])){
+        resource = associated_resources_search[key];
       }
-      // create the payload of attributes of the event we are interested. 
-      let payload = {
-        "client_id": event.clientId,
-        "timestamp":event.timestamp,
-        "page_url":event.context.document.location.href,
-        "associated_resource": resource
-      }
-      console.log('[Pixel] Page viewed', payload);
-    });
-    
-    analytics.subscribe('checkout_completed', (event) => {
-      let payload = {
-        "client_id": event.clientId,
-        "timestamp": event.timestamp,
-        "products": event.data.checkout.lineItems
-      }
-      console.log('[Pixel] Checkout Completed', payload);
-    }); 
-    analytics.subscribe('product_added_to_cart', (event) => {
-      console.log('debug: product_added_to_cart: ', event)
-      let payload = {
-        "client_id": event.clientId,
-        "timestamp": event.timestamp,
-        "product": event.data.cartLine.merchandise, 
-        "add_to_cart_source": event.context.document.referrer
-      }
-      console.log('[Pixel] Product Added to Cart', payload);
-    });
-    analytics.subscribe('checkout_started', (event) => {
-      let payload = {
-        "client_id": event.clientId,
-        "timestamp": event.timestamp,
-        "products": event.data.checkout.lineItems,
-
-      }
-      console.log('[Pixel] Checkout Started', payload);
-    });
+    }
+    // create the payload of attributes of the event we are interested. 
+    let payload = {
+      "client_id": event.clientId,
+      "timestamp":event.timestamp,
+      "page_url":event.context.document.location.href,
+      "associated_resource": resource
+    }
+    console.log('[Pixel] Page viewed', payload);
+  });
+  
+  analytics.subscribe('checkout_completed', (event) => {
+    let payload = {
+      "client_id": event.clientId,
+      "timestamp": event.timestamp,
+      "products": event.data.checkout.lineItems
+    }
+    console.log('[Pixel] Checkout Completed', payload);
+  }); 
+  analytics.subscribe('product_added_to_cart', (event) => {
+    console.log('debug: product_added_to_cart: ', event)
+    let payload = {
+      "client_id": event.clientId,
+      "timestamp": event.timestamp,
+      "product": event.data.cartLine.merchandise, 
+      "add_to_cart_source": event.context.document.referrer
+    }
+    console.log('[Pixel] Product Added to Cart', payload);
+  });
+  analytics.subscribe('checkout_started', (event) => {
+  let payload = {
+    "client_id": event.clientId,
+    "timestamp": event.timestamp,
+    "products": event.data.checkout.lineItems,
+    }
+    console.log('[Pixel] Checkout Started', payload);
+  });
 
 });
