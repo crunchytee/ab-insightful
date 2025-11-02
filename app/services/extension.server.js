38c4131e72f581716a9b5e045467eeb8a6d9e30f
Tosh RB <86034433+crunchytee@users.noreply.github.com>
2025-11-01 20:34:08 -0700

    make app url dynamic/accessible from store site

--- app/services/extension.server.js
+++ app/services/extension.server.js
@@ -1,12 +1,17 @@
 import { authenticate } from "../shopify.server";
-export async function registerWebPixel({request}){
-    const {admin} = await authenticate.admin(request);
-      // [ryan] Code for automatically registering the Web-Pixel extension with the shop. 
+export async function registerWebPixel({ request }) {
+  const { admin } = await authenticate.admin(request);
+  // [ryan] Code for automatically registering the Web-Pixel extension with the shop.
 
-    const response = await admin.graphql( 
-      `#graphql
-        mutation {
-        webPixelCreate(webPixel: { settings: "{\\"accountID\\":\\"123\\"}" }) {
+  const settings = {
+    accountID: "123",
+    appUrl: process.env.SHOPIFY_APP_URL,
+  };
+
+  const response = await admin.graphql(
+    `#graphql
+        mutation($settings: JSON!) {
+        webPixelCreate(webPixel: { settings: $settings }) {
           userErrors {
             code
             field
@@ -18,12 +23,20 @@ export async function registerWebPixel({request}){
           }
         }
       }
-  `
+      `,
+    {
+      variables: {
+        settings: settings,
+      },
+    },
+  );
+  // error check
+  const responseAsJSON = await response.json();
+  if (responseAsJSON.data?.webPixelCreate?.userErrors?.length > 0) {
+    console.error(
+      "An error occurred while trying to register the Web Pixel App Extension:",
+      responseAsJSON.data.webPixelCreate.userErrors,
     );
-// error check
-    const responseAsJSON = await response.json()
-    if(responseAsJSON.data?.webPixelCreate?.userErrors?.length > 0){
-      console.error('An error occurred while trying to register the Web Pixel App Extension:', responseAsJSON.data.webPixelCreate.userErrors)
-    }
-    return null
+  }
+  return null;
 }
