ef57459478a95e02a676d75878a7dac3323313b4
ryanmart25 <80070431+ryanmart25@users.noreply.github.com>
2025-11-13 13:13:21 -0800

    fixed logged messages

--- app/services/cookie.server.js
+++ app/services/cookie.server.js
@@ -1,13 +1,16 @@
 import db from "../db.server";
 
- // TODO refactor so methods all follow the flow of updateLatestSession
-export async function getUserbyID(customer_id){
-  console.log("[cookie.server.js] looking up user with id: ", customer_id ?? "[ERROR] cookie.server::getUserbyID: userdata is undefined.");
-  if(customer_id){
+// TODO refactor so methods all follow the flow of updateLatestSession
+export async function getUserbyID(customer_id) {
+  console.log(
+    "[cookie.server.js] looking up user with id: ",
+    customer_id ?? "[ERROR] cookie.server::getUserbyID: userdata is undefined.",
+  );
+  if (customer_id) {
     const customer = await db.user.findUnique({
-        where : {
-            shopifyCustomerID: customer_id,
-        },
+      where: {
+        shopifyCustomerID: customer_id,
+      },
     });
     console.log("found customer: ", customer);
     return customer;
@@ -16,34 +19,41 @@ export async function getUserbyID(customer_id){
   return null;
 }
 
-export async function createUser(userdata){
-    console.log("[cookie.server.js] creating new user: ", userdata ?? "[ERROR] cookie.server::createUser: userdata is undefined.");
-    if(userdata){
-      const result = await db.user.create({
-        data: {
-          ...userdata,
-        }
-      });
-      console.log("Created User: ", result);
-      return result;
-    }
-    return null;
+export async function createUser(userdata) {
+  console.log(
+    "[cookie.server.js] creating new user: ",
+    userdata ?? "[ERROR] cookie.server::createUser: userdata is undefined.",
+  );
+  if (userdata) {
+    const result = await db.user.create({
+      data: {
+        ...userdata,
+      },
+    });
+    console.log("Created User: ", result);
+    return result;
+  }
+  return null;
 }
-export async function updateLatestSession(userdata){
-  // should refactor to be a more generic "update user data" where this function is responsible for A) figuring out what has changed and B) sendign a PATCH to the database to update just those fields. 
-  if(!userdata){
-    console.log("[cookieStore.server.js] creating new user:",userdata ??" [ERROR] cookieStore,server::updateLatestSession: userdata is undefined.");
+export async function updateLatestSession(userdata) {
+  // should refactor to be a more generic "update user data" where this function is responsible for A) figuring out what has changed and B) sendign a PATCH to the database to update just those fields.
+  if (!userdata) {
+    console.log(
+      "[cookieStore.server.js] [ERROR] cookieStore,server::updateLatestSession: userdata is undefined.",
+    );
     return null;
-  }else{
+  } else {
+    console.log(
+      `Updating User's latest session...\nUser ID: ${userdata.shopifyCustomerID}\n New Session Date: ${userdata.latestSession}`,
+    );
     const result = await db.user.update({
       where: {
-        shopifyCustomerID: userdata.shopifyCustomerID
+        shopifyCustomerID: userdata.shopifyCustomerID,
       },
       update: {
         latestSession: userdata.latestSession,
-      }
+      },
     });
     return result;
   }
 }
-
